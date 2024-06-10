b21e0c91dcd5eb0ffc70b883ee8053af1d812874
KimJinung <KimJinung@outlook.com>
2024-06-11 00:16:29 +0900

    chore

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -1,3 +1,39 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+# Python 함수 정의
+def hello_world():
+    print("Hello, World!")
+
+# 기본 DAG 설정
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# DAG 정의
+with DAG(
+    'simple_hello_world_dag',
+    default_args=default_args,
+    description='A simple hello world DAG',
+    schedule_interval=timedelta(days=1),
+    catchup=False,
+) as dag:
+
+    # PythonOperator 정의
+    hello_task = PythonOperator(
+        task_id='hello_task',
+        python_callable=hello_world,
+    )
+
+    hello_task  # 태스크 정의
+
  import datetime
 
  from airflow import DAG
@@ -8,5 +44,5 @@
      start_date=datetime.datetime(2021, 1, 1),
      schedule="@daily",
  )
- 
+
  EmptyOperator(task_id="task", dag=my_dag)
