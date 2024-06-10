9e724b2627a5831638099becb0f87a2914520120
KimJinung <KimJinung@outlook.com>
2024-06-11 00:16:50 +0900

    chore

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -33,16 +33,3 @@ with DAG(
     )
 
     hello_task  # 태스크 정의
-
- import datetime
-
- from airflow import DAG
- from airflow.operators.empty import EmptyOperator
-
- my_dag = DAG(
-     dag_id="my_dag_name",
-     start_date=datetime.datetime(2021, 1, 1),
-     schedule="@daily",
- )
-
- EmptyOperator(task_id="task", dag=my_dag)
